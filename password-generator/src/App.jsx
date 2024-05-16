6d64ad24eb9d6ca9367f1bc5d3b0bab070a848d3
Rakesh Kumar <rakeshspm2323@gmail.com>
2024-05-16 15:09:24 +0530

    Revise Password Generator

--- password-generator/src/App.jsx
+++ password-generator/src/App.jsx
@@ -1,91 +1,82 @@
 import React, { useCallback, useEffect, useRef, useState } from 'react'
 
-
 const App = () => {
-
-  const [length,setLength] = useState(8)
-  const [numAllowed,setNumAllowed] = useState(true)
-  const [charAllowed,setCharAllowed] = useState(true)
-  const [password,setPassword] = useState("")
-
-// UseRef hook
-  const passwordRef = useRef(null)
-
-  const passwordGenerator = useCallback(()=>{
+   const [length,SetLength] = useState(8)
+   const [numAllowed,SetnumAllowed] = useState(true)
+   const [charAllowed,SetcharAllowed] = useState(true)
+   const [password,setPassword] = useState("")
+  //password references
+  const passwordref = useRef(null)
+  //password genrator
+   const passwordGenerator = useCallback(()=>{
     let pass = ""
     let str = "ABCDEFGHIGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-    if(numAllowed)str += "0123456789"
-    if(charAllowed)str += "!@#$%^&*-_~"
+    if(numAllowed) str += "0123456789"
+    if(charAllowed) str += "!@#$%^&*-_~"
 
     for(let i=1; i<=length; i++){
       let char = Math.floor(Math.random() * str.length +1)
       pass += str.charAt(char)
     }
     setPassword(pass)
-    },[length,numAllowed,charAllowed,setPassword])
-
-    const copyPassToClipboard = useCallback(()=>{
-        // window.navigator.clipboard.writeText(password)  --> // select option is not provide so use passwordRef (useRef hook)
-        passwordRef.current?.select()
-        passwordRef.current?.setSelectionRange(0,15) // select upto 15 char
-    },[password])
-
-    useEffect(()=>{
-      passwordGenerator()
-    },[length,numAllowed,charAllowed,passwordGenerator])
-
+   },[length,numAllowed,charAllowed,setPassword])
+   //password copy
+   const copyPassTOClipboard = useCallback(()=>{
+    passwordref.current?.select()  //select all password for copy
+    passwordref.current?.setSelectionRange(0,15) // upto 15 char to copy 
+   },[password])
+   //call password generator
+   useEffect(()=>{
+    passwordGenerator()
+   },[length,numAllowed,charAllowed,passwordGenerator])
   return (
     <>
-      <div className='w-full max-w-md mx-auto bg-gray-700 rounded-lg shadow-md px-4 my-8 pb-2 '>
-          <h1 className='text-center text-white my-3 pt-2'>Password Generator</h1>
-          <div className='flex shadow rounded-lg overflow-hidden mb-4'>
-              <input type="text"
-                value={password}
-                className='outline-none w-full py-1 px-3'
-                placeholder='password'
-                readOnly  ref={passwordRef}/>
-              <button className='outline-none bg-blue-500 text-white px-0.5 py-3 shrink-0'
-                      onClick={copyPassToClipboard}>
-                Copy
+      <div className='w-[500px] h-[250px] bg-gray-600 m-auto mt-40  text-white p-3 rounded-md'>
+          <div className='w-full h-[50px]  mb-2 font-extrabold flex justify-center items-center'>
+            Password Generator
+            </div>
+          <div className='w-full h-[80px]  flex justify-center items-center'>
+           <input type="text" placeholder='password'  value={password} readOnly  ref={passwordref}
+           className='outline-none w-full py-2 px-3 rounded-l-lg text-black' />
+            <button className='px-2 py-2 bg-blue-600 rounded-r-lg' onClick={copyPassTOClipboard}>
+              Copy
               </button>
           </div>
-          <div className='flex text-sm gap-x-2'>
-              <div className='flex items-center gap-x-1'>
-                  <input 
-                  type="range"
-                  min={8}
-                  max={50}
-                  value={length}
-                  id='length'
-                  className='cursor-pointer'
-                  onChange={(e)=>{setLength(e.target.value)}} />
-                 <label htmlFor="length" className='text-white'>Length:{length}</label>
+          <div className='w-full h-[80px] flex justify-center items-center gap-5'>
+              <div>
+                <input type="range" 
+                min={8} max={50} 
+                value={length} 
+                id="length" 
+                className='cursor-pointer'
+                onChange={(e)=>{SetLength(e.target.value)}}/>
+                <label htmlFor="length" className='cursor-pointer'>Length:{length}</label>
               </div>
-              <div className='flex items-center gap-x-1'>
-                  <input 
-                  type="checkbox"
-                  defaultChecked={numAllowed}
-                  id='numberInput'
-                  className='cursor-pointer'
-                  onChange={()=>{
-                    setNumAllowed((prev)=>!prev) //!prev that means reverse value 
-                    }} />
-                 <label htmlFor="numberInput" className='text-white'>Number</label>
+              <div>
+                <input type="checkbox" 
+                defaultChecked={numAllowed} 
+                id="numInput" 
+                className='cursor-pointer'
+                onChange={()=>{
+                  SetnumAllowed((prev)=>!prev)
+                }}/>
+                <label htmlFor="numInput"className='cursor-pointer'>Number</label>
               </div>
-              <div className='flex items-center gap-x-1'>
-                  <input 
-                  type="checkbox"
-                  defaultChecked={charAllowed}
-                  id='charInput'
-                  className='cursor-pointer'
-                  onChange={()=>{
-                    setCharAllowed((prev)=>!prev) //!prev that means reverse value 
-                    }} />
-                 <label htmlFor="charInput" className='text-white'>Character</label>
+              <div>
+                <input type="checkbox" 
+                defaultChecked={charAllowed} 
+                id="charInput"
+                className='cursor-pointer'
+                onChange={()=>{
+                  SetcharAllowed((prev)=>!prev)
+                }} />
+                <label htmlFor="charInput" className='cursor-pointer'>Character</label>
               </div>
           </div>
       </div>
+    
     </>
+   
   )
 }
 
